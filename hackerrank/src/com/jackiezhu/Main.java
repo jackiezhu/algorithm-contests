de8880c02be0d8c9fab15d6aee91c4e1f4aadc15
JackieZhu <zhuzewei0927@gmail.com>
2015-03-29 14:47:13 +0800

    two sum

--- hackerrank/src/com/jackiezhu/Main.java
+++ hackerrank/src/com/jackiezhu/Main.java
@@ -1,8 +1,22 @@
 package com.jackiezhu;
 
 public class Main {
-
+    public static int firstMissingPositive(int[] A) {
+        for(int i=0; i<A.length; i++) {
+            while(A[i] > 0 && A[i] <= A.length && A[A[i]-1] != A[i]) {
+                int temp = A[i];
+                A[i] = A[A[i]-1];
+                A[A[i]-1] = temp;
+            }
+        }
+        for(int i=0; i<A.length; i++) {
+            if(A[i] != i+1) return i+1;
+        }
+        return A.length+1;
+    }
     public static void main(String[] args) {
 	// write your code here
+        int[] A =  {1,2};
+        System.out.println(firstMissingPositive(A));
     }
 }
